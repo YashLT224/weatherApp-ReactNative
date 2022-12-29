fdab215e50dd8124cbc435968c92075415b2b8f7
Yash Verma <yashv@lambdatest.com>
2022-12-29 13:17:35 +0530

    weather app

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React,{useState,useEffect} from 'react';
+import {ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+ import Weather from './components/Weather';
+ import SearchBar from './components/Searchbar';
+ // get your api key from https://openweathermap.org/current#current_JSON
+const API_KEY=" "
 export default function App() {
+
+  const [weatherData,setWeatherData]=useState(null);
+  const [loaded,setLoaded]=useState(true);
+
+
+  async function fetchWeatherData(cityName){
+    setLoaded(false);
+    const API= `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+    try{
+      const response=await fetch(API);
+      if(response.status==200){
+        
+        const data = await response.json();
+                setWeatherData(data);
+      }else{
+        setWeatherData(null);
+      }
+      setLoaded(true);
+
+
+    }
+    catch(e){
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+     fetchWeatherData('mumbai');
+     console.log(weatherData)
+  },[])
+
+  if(!loaded){
+    return(
+      <View style={styles.container}>
+        <ActivityIndicator color='red' size={36} />
+      </View>
+    )
+  }
+  else if(!weatherData){
+    return (
+      <View style={styles.container}>
+          <SearchBar fetchWeatherData={fetchWeatherData}/>
+          <Text style={styles.primaryText}>City Not Found! Try Different City</Text>
+      </View>
+  )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"  backgroundColor='purple'/>
+     
+      <Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData}/>
     </View>
   );
 }
@@ -17,4 +69,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  primaryText: {
+    margin: 20,
+    fontSize: 28
+}
 });
---
+++ assets/bgImages/index.js
@@ -0,0 +1,8 @@
+ 
+import haze from './haze.jpg'
+import rainy from './rainy.jpeg'
+import snow from './snow.jpeg'
+import sunny from './sunny.jpg'
+ 
+
+export { haze, rainy, snow, sunny };
